92693ee269e41f830903c683c5e1a5da9e6f2502
Vince Weel <vince.weel.io@outlook.com>
2021-07-28 22:39:55 +0800

    Updates

---
+++ custom/config.js
@@ -0,0 +1,14 @@
+export default `
+[notion]
+# 'issues' | 'branch' | 'wiki'
+source = 'issues'
+
+[repository]
+name = 'GNotion'
+owner = 'vinceweel'
+
+[authorization]
+token = 'XTFXWDL0toNgsbYX69HdkYsCyp4sfN2C5Ovc'
+client_id = ''
+client_secret = ''
+`
--- custom/main.js
+++ custom/main.js
@@ -1,10 +1,18 @@
-import settings from './settings.js'
+import tomlSettings from './settings.js'
+import tomlConfig from './config.js'
+
+const GNotion = {}
 
 const initCustom = (context = {}) => {
-  const { tomlParser } = context
-  const toml = tomlParser(settings)
-  console.log(toml)
-  
+  const {
+    runtime: { parse },
+  } = context
+
+  const settings = parse(tomlSettings)
+  const config = parse(tomlConfig)
+
+  Object.assign(GNotion, { settings, config })
 }
 
-Object.assign(globalThis, { GNotion: { initCustom } })
+Object.assign(GNotion, { initCustom })
+Object.assign(globalThis, { GNotion })
